4a31cabece811d2ae8828ee334a1fa046e223b3c
Steph <108551327+s-hens@users.noreply.github.com>
2023-12-07 22:06:21 +0000

    Solve day 1 part 1. Set up day 1 part 2

---
+++ src/day1B.js
@@ -0,0 +1,12 @@
+/////////////////////////
+// Advent of Code 2023 //
+// Day 1 Part 2        //
+/////////////////////////
+
+// Dataset
+import { data1 } from "./day1A";
+
+let answer1B = "idk lol"
+
+// Exports
+export { answer1B } 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+/////////////////////////
+// Advent of Code 2023 //
+/////////////////////////
+
+// Imports
+import { answer1A } from "./day1A";
+import { answer1B } from "./day1B";
+
+// Print answers
+console.log(answer1A);
+console.log(answer1B);
