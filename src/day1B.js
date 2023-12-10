8001b418a7abc36c3a71891dbe5688052977d93d
Steph <108551327+s-hens@users.noreply.github.com>
2023-12-10 21:34:14 +0000

    Solve day 1 part 2

--- src/day1B.js
+++ src/day1B.js
@@ -24,20 +24,35 @@ const testData = [
 
 let regex = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
 
-testData.forEach(string => {
-    
+let digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+let answer1B = 0;
+
+function convertToNum(string) {
+    if (!Number.isNaN(Number(string))) {
+        return Number(string);
+    } else {
+        return digits.indexOf(string) + 1;
+    }
+}
+
+data1.forEach(string => {
     // Use matchAll because it works with global matching
     // Convert to array because matchAll returns an iterator
     // Get just the info we need from that array
-
     let matches = Array.from(string.matchAll(regex), (match) => match[1]);
     let digit1 = matches.at(0);
     let digit2 = matches.at(matches.length - 1);
-    console.log(string, digit1, digit2);
-
+    // Convert to Numbers
+    digit1 = convertToNum(digit1);
+    digit2 = convertToNum(digit2);
+    // The calibration value is digit1 and digit 2 concatenated
+    let calibrationValue = Number(`${digit1}${digit2}`);
+    // The final answer is the sum of all calibration values
+    answer1B = answer1B + calibrationValue;
 });
 
-const answer1B = "idk";
+console.log(answer1B);
 
 // Exports
 export { answer1B };
